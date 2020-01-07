1697adbac94ca81e204be6c3971bbe3ba82ebfbd
Joanajua <J.Ayan@ucas.ac.uk>
2020-01-07 11:06:18 +0000

    Author, name added

---
+++ js/common.js
@@ -0,0 +1,9 @@
+'use strict';
+// JavaScript containing shared constants, for use in all pages.
+
+var backendIp = '34.248.103.39';
+var token = '16d6997c-d90c-43d7-84f5-2c6535c629ce';
+
+function buildUrl(path) {
+    return 'http://' + backendIp + '/images' + path + '?token=' + token;
+}
---
+++ js/index.js
@@ -0,0 +1,51 @@
+'use strict';
+// JavaScript for use with the index page.
+
+function loadRandomImage() {
+    fetch(buildUrl('/random'))
+        .then(function (response) {
+            if (response.status !== 200) {
+                throw new Error('Request return status code !== 200: ' + response.status + ' - ')
+            }
+            return response.json();
+        })
+        .then(function (json) {
+            console.log('Request to /random succeeded: ');
+            console.log(json);
+
+            var mainImage = $('#main-image');
+            mainImage.attr('src', json.url);
+            mainImage.attr('alt', 'Photo Competition image, ' + json.name);
+           
+
+            var author = $('#author');
+            author.text(json.author);
+
+            var name = $('#name');
+            name.text(json.name);
+
+            var license = $('#license');
+            license.text(json.license);
+
+            var id = $('#id');
+            id.text(json.id);
+
+            var votes = $("#votes");
+            votes.text(json.score);
+            
+           
+
+        })
+        .catch(function (err) {
+            console.error('Request to /random failed: ', err);
+        });
+        
+}
+
+
+$(function () {
+    loadRandomImage();
+});
+
+
+
